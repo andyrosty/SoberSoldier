a31c9e8bfc98dd5c27233caa997acaecf98e3460
Andrew Acheampong <gorgi.andy@gmail.com>
2025-03-11 00:39:09 -0400

    application.properties update

---
+++ src/main/java/com/andrew/sobersoldier/SoberSoldierApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.sobersoldier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoberSoldierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoberSoldierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrew/sobersoldier/SoberSoldierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.sobersoldier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoberSoldierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
