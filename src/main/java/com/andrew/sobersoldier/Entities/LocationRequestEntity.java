007686a0357779415cccb701d8e0ecee2e0eaf0d
Andrew Acheampong <gorgi.andy@gmail.com>
2025-03-11 01:36:03 -0400

    LocationRequestEntity.java created

--- src/main/java/com/andrew/sobersoldier/Entities/LocationRequestEntity.java
+++ src/main/java/com/andrew/sobersoldier/Entities/LocationRequestEntity.java
@@ -1,5 +1,30 @@
 package com.andrew.sobersoldier.Entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
 public class LocationRequestEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long requestId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private LocalDateTime requestTime = LocalDateTime.now();
+
+    @Column(nullable = false, length = 50)
+    private String status = "PENDING";
 }
