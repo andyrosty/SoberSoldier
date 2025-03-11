bebe0d2c241cc28eae85d9ae583a9c84e147fca7
Andrew Acheampong <gorgi.andy@gmail.com>
2025-03-11 01:10:41 -0400

    UserEntity.java creation

---
+++ src/main/java/com/andrew/sobersoldier/Entities/UserEntity.java
@@ -0,0 +1,32 @@
+package com.andrew.sobersoldier.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false,unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String phone;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
